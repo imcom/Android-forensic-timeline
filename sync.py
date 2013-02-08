acc0dbfe1fcf8c1a3b83a13bc130f048a624fb97
Yu Jin <jinyu.bjut@gmail.com>
2013-02-08 16:00:39 +0100

    sync py, sh script almost done

--- sync.py
+++ sync.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python
 
-#TODO write a script to sync java code in eclipse workspace
-
 from os import path
 import sys
+import glob
+import subprocess
+import shutil
+
+def syncFile(src, dst):
+    try:
+        f4sync = glob.glob(src + "*.sh")
+        f4sync.extend(glob.glob(src + "*.py"))
+        for f in f4sync:
+            shutil.copy(f, dst)
+        return len(f4sync)
+    except e:
+#TODO print error info
+        return -1
+
+programs = ["android_logs",
+            "android_sqlite",
+            "fs_timeline",
+            "jsonizer"
+           ]
 
 git_dir = path.abspath(path.curdir) + path.sep
 home_dir = path.expanduser('~') + path.sep
 
-bak_dir = home_dir + path.sep + "Desktop" + path.sep + "Forensics" + path.sep + "source_code" + path.sep
-dev_dir
-eclipse_workspace
+bak_dir = home_dir + "Desktop/Forensics/source_code"
+dev_dir = home_dir + "Documents/code/lab/forensics/android"
+eclipse_workspace = home_dir + "Documents/code/workspace/Forensics"
 
 if not path.isdir(git_dir):
     print "Git directory is not found. Terminating..."
@@ -19,3 +37,24 @@ if not path.isdir(git_dir):
 do_backup = path.isdir(bak_dir)
 if not do_backup:
     print "Skipping backup process since backup directory is not found... "
+else:
+    print "Backup directory is set to [%s]" % (bak_dir)
+
+print "Start syncing source code to Git repository... [%s]" % (git_dir)
+
+for program in programs:
+    src = dev_dir + path.sep + program + path.sep
+    dst = git_dir + program + path.sep
+    print "syncing %s ...\t" % program,
+    rtn = syncFile(src, dst)
+    if program is 'android_sqlite':
+        rtn *= syncFile(src + "modules/", dst + "modules/")
+    if rtn > 0:
+        print "ok"
+    else:
+        print "Failed to sync %s" % program
+
+
+
+
+
