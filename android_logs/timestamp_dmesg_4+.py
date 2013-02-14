367ad15d2aa656b33ca09a09a34ce9d791a8f158
Yu Jin <jinyu.bjut@gmail.com>
2013-02-14 06:49:46 +0100

    logcat parser and support for android 4.0+

---
+++ android_logs/timestamp_dmesg_4+.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys
+import re
+import os
+import json
+from datetime import timedelta, datetime
+
+btime = datetime.fromtimestamp(float(sys.argv[1]))
+log_file = sys.argv[2]
+save_to = sys.argv[3]
+
+time_data_mask = re.compile("^.*\[(?P<etime>\s*\d+\.\d+)\](?P<event>.*$)")
+
+if not os.path.isdir(save_to):
+    #TODO write the following info to log file
+    #print "creating result directory... " + save_to
+    os.makedirs(save_to)
+
+new_log_file = open(save_to + os.path.sep + os.path.basename(log_file), 'w')
+
+with open(log_file) as dmesg:
+    lines = dmesg.read().strip()
+    for line in lines.split('\n'):
+        #FIXME on android 4.0+ log entry starts with '<#>'
+        if not line.startswith('[') and not line.startswith('<'):
+            continue
+        match = time_data_mask.match(line) 
+        if match is not None:
+            seconds = int(match.groupdict().get('etime').split('.')[0])
+            nseconds = int(match.groupdict().get('etime').split('.')[1])
+            mseconds = int(round(nseconds / 1000))
+            event = match.groupdict().get('event')
+            timestamp = btime + timedelta(seconds=seconds, microseconds=mseconds)
+            buf = {'date':timestamp.strftime('%s'), 'event':event.strip()}
+            json.dump(buf, new_log_file)
+            new_log_file.write('\n')
+        else:
+            #TODO error message
+            print 'invalid log entry found', line
+
+new_log_file.close()
+print 'ok'
+
+
+
+
---
+++ android_logs/timestamp_logcat.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import re
+from datetime import datetime
+import time
+import sys
+import json
+import os
+
+read_from = sys.argv[1]
+save_to = sys.argv[2]
+
+cur_year = datetime.utcnow().year
+log_files = ['main.log', 'system.log', 'events.log', 'radio.log']
+time_data_mask = re.compile("^(?P<etime>\d+-\d+\s[\d+:]+\d+)(.\d+)\s(?P<event>.*)$")
+
+for log_file in log_files:
+    print 'parsing log file [' + log_file + '] ...',
+    new_log_file = open(save_to + os.path.sep + log_file, 'w')
+    with open(read_from + os.path.sep + log_file) as src:
+        lines = src.read().strip()
+        for line in lines.split('\n'):
+            match = time_data_mask.match(line)
+            if match is not None:
+                etime = str(cur_year) + '-' + match.groupdict()["etime"]
+                event = match.groupdict()["event"]
+                timestamp = time.mktime(datetime.strptime(etime, "%Y-%m-%d %H:%M:%S").timetuple())
+                buf = {'date':timestamp, 'event':event.strip()}
+                json.dump(buf, new_log_file)
+                new_log_file.write('\n')
+            else:
+                #TODO move this output to log file 
+                print 'invalid log entry found', line
+
+        new_log_file.close()
+        print 'ok'
+
+
+
+
