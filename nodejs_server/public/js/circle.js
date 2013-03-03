d4465ed4e385eee3cdec1fda3cca68f680c28d32
imcom <jinyu.bjut@gmail.com>
2013-03-03 16:34:59 +0100

    added grid lines adjusted interval length adjusted y axis labels position

--- nodejs_server/public/js/circle.js
+++ nodejs_server/public/js/circle.js
@@ -1,15 +1,16 @@
 var svg = d3.select("#timeline")
         .append("svg")
-        .attr("width", 500)
-        .attr("height", 500);
+        .attr("width", "100%")
+        .attr("height", "1500"); // the height of the page should be a variable
 
 var dataset = [
-    [50, 30],
-    [300, 30],
-    [50, 100],
-    [300, 110],
-    [50, 170],
-    [300, 190],
+    [50, 1111130],
+    [300, 1111130.2],
+    [350, 1111130.4],
+    [50, 1111132],
+    [300, 1111133],
+    [50, 1111133.8],
+    [300, 1111136],
 ];
 
 var data_desc = [
@@ -19,6 +20,7 @@ var data_desc = [
     ["event", "4"],
     ["event", "5"],
     ["event", "6"],
+    ["event", "7"],
 ];
 
 var xScale = d3.scale.linear()
@@ -26,14 +28,14 @@ var xScale = d3.scale.linear()
                         d3.min(dataset, function(data) { return data[0]; }),
                         d3.max(dataset, function(data) { return data[0]; })
                     ])
-             .range([50, 200]);
+             .range([200, 400]);
 
 var yScale = d3.scale.linear()
              .domain([
                         d3.min(dataset, function(data) { return data[1]; }),
                         d3.max(dataset, function(data) { return data[1]; })
                     ])
-             .range([50, 200]);
+             .range([50, 1000]);
 
 svg.selectAll("circle")
     .data(dataset)
@@ -45,14 +47,13 @@ svg.selectAll("circle")
     .attr("cy", function(data) {
         return yScale(data[1]);
     })
-    .attr("r", function(data) {
-        return Math.sqrt(500 - data[1]);
-    });
+    .attr("r", 3);
 
-svg.selectAll("text")
+svg.selectAll("text.desc")
     .data(data_desc)
     .enter()
     .append("text")
+    .attr("class", "desc")
     .text(function(data) {
         return data[0] + "," + data[1];
     })
@@ -64,6 +65,32 @@ $.each(dataset, function(index) {
     text_fields[index].setAttribute("y", yScale(dataset[index][1]));
     text_fields[index].setAttribute("id", data_desc[index][1]);
 });
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("right")
+    .ticks(5);
+
+svg.append("g")
+    .attr("class", "time-axis")
+    .call(yAxis);
+
+var labels = $("g.tick");
+$.each(labels, function(index) {
+    labels[index].childNodes[1].setAttribute("dy", "-.5em");
+});
+
+var rules = svg.selectAll("g.rule")
+    .data(yScale.ticks(5))
+    .enter()
+    .append("g")
+    .attr("class", "rule");
+
+rules.append("line")
+    .attr("y1", yScale)
+    .attr("y2", yScale)
+    .attr("x1", 0)
+    .attr("x2", "100%");
 
 /*The second timeline*/
 var svg_1 = d3.select("#events")
@@ -79,3 +106,7 @@ svg_1.append("circle")
     .attr("cy", 50)
     .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
     .on("mouseout", function(){d3.select(this).style("fill", "white");});
+    
+
+
+
