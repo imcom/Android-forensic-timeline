33d0efcc123e8a91e1fac6f180ef9ec979672bdc
imcom <jinyu.bjut@gmail.com>
2013-03-04 14:01:19 +0100

    added mongodb module for fetching data from database callback function added for reserving the data fetched TODO data manipulation, dealing with json data and map the data to graph

--- nodejs_server/public/js/mongo_helper.js
+++ nodejs_server/public/js/mongo_helper.js
@@ -2,29 +2,36 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/imcom');
 
-mongoose.connection.db.collectionNames(function(err, names){
-    console.log(names);
-    mongoose.disconnect();
-});
-
 var LOG_SCHEMA = mongoose.Schema(
     {
-        date: 'number',
-        msg: 'string',
-        object: 'string',
-        pid: 'string',
-        level: 'string'
-    },
-    {
-        collection: ''
-    {
+        date: Number,
+        msg: String,
+        object: String,
+        pid: String,
+        level: String
+    }
 );
 
-var RADIO = mongoose.model('Radio', LOG_SCHEMA);
+var log_collections = ['dmesg', 'radio', 'events', 'main', 'system'];
+
+var log_records = {};
+var counter = 1;
+
+log_collections.forEach( function(collection) {
+    var model = mongoose.model(collection, LOG_SCHEMA, collection);
+    model.find(null, function(err, res) {
+        log_records[collection] = res;
+        console.log(counter + " " + collection);
+        if (counter == log_collections.length) {
+            onCompletion(log_records);
+        }
+        counter += 1;
+    });
+});
 
 
-/*
+function onCompletion(log_records) {
+    console.log(log_records['radio'].length);
+    mongoose.disconnect();
+}
 
-{ "_id" : ObjectId("5133c1692e61db04693b3d73"), "date" : 1362342357, "msg" : "[UNSL]< UNSOL_SIGNAL_STRENGTH {13, 99, -1, -1, -1, -1, 0, -1, -1, 0, 0, 0, 0, 0}\r", "object" : "RILJ", "pid" : " 1625", "level" : "D" }
-
-*/
