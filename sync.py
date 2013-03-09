15a40e21e399f00d73b6afe4814793ee4b17501e
Yu Jin <jinyu.bjut@gmail.com>
2013-03-09 19:00:40 +0100

    separate sync and backup functions

--- sync.py
+++ sync.py
@@ -21,7 +21,8 @@ def syncFile(src, dst):
 programs = ["android_logs",
             "android_sqlite",
             "fs_timeline",
-            "jsonizer"
+            "jsonizer",
+            "nodejs_server"
            ]
 
 git_dir = path.abspath(path.curdir) + path.sep
@@ -35,41 +36,51 @@ if not path.isdir(git_dir):
     print "Git directory is not found. Terminating..."
     sys.exit(-1)
 
+do_sync = False
 do_backup = path.isdir(bak_dir)
 if not do_backup:
     print "Skipping backup process since backup directory is not found... "
 else:
     print "Backup directory is set to [%s]" % (bak_dir)
 
-print "Start syncing source code to Git repository... [%s]" % (git_dir)
-
-for program in programs:
-    src = dev_dir + path.sep + program + path.sep
-    dst = git_dir + program + path.sep
-    print "syncing %s ...\t" % program,
-    rtn = syncFile(src, dst)
-    if program is 'android_sqlite':
-        rtn *= syncFile(src + "modules/", dst + "modules/")
-    if rtn > 0:
-        print "ok"
+def sync_source_code():
+    print "Start syncing source code to Git repository... [%s]" % (git_dir)
+    
+    for program in programs:
+        src = dev_dir + path.sep + program + path.sep
+        dst = git_dir + program + path.sep
+        print "syncing %s ...\t" % program,
+        rtn = syncFile(src, dst)
+        if program is 'android_sqlite':
+            rtn *= syncFile(src + "modules/", dst + "modules/")
+        if rtn > 0:
+            print "ok"
+        else:
+            print "Failed to sync %s" % program
+    
+    print "Start syncing [%s] project... " % eclipse_workspace,
+    src = eclipse_workspace + path.sep
+    rtn = subprocess.call(["cp", "-r", src + "AndroidManifest.xml", src + "res", src + "src", git_dir + "java/"])
+    if rtn is not 0:
+        print "Failed to sync [%s] project" % eclipse_workspace
     else:
-        print "Failed to sync %s" % program
+        print "ok"
 
-print "Start syncing [%s] project... " % eclipse_workspace,
-src = eclipse_workspace + path.sep
-rtn = subprocess.call(["cp", "-r", src + "AndroidManifest.xml", src + "res", src + "src", git_dir + "java/"])
-if rtn is not 0:
-    print "Failed to sync [%s] project" % eclipse_workspace
-else:
-    print "ok"
+def backup():
+    print 'Start backing up Git repo...'
+    archive = shutil.make_archive(bak_dir + "/bak_" + time.strftime("%Y%m%d%H%M"), "gztar", git_dir)
+    if archive is not None:
+        print 'Git repo has backed up to [%s]' % archive
+        print 'Sync has completed'
+    else:
+        print 'Failed to back up Git repo.\nQuitting...'
 
-print 'Start backing up Git repo...'
-archive = shutil.make_archive(bak_dir + "/bak_" + time.strftime("%Y%m%d%H%M"), "gztar", git_dir)
-if archive is not None:
-    print 'Git repo has backed up to [%s]' % archive
-    print 'Sync has completed'
-else:
-    print 'Failed to back up Git repo.\nQuitting...'
+if do_backup:
+    backup()
+
+if do_sync:
+    sync_source_code()
+
 
 
 
