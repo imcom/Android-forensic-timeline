7f865c1c6eee9682543f93c24a515bcb46b9986d
Yu Jin <jinyu.bjut@gmail.com>
2013-03-14 19:50:53 +0100

    found `istat` will add timezone info on some datetime, remove the timezone info, convert the datetime to UTC timestamp

--- fs_timeline/inode_times.py
+++ fs_timeline/inode_times.py
@@ -9,7 +9,7 @@ import re
 inode_file = open(sys.argv[1])
 output_file = open(sys.argv[2], 'w')
 
-date_mask = re.compile("\d+-\d+-\d+ \d+:\d+:\d")
+date_mask = re.compile("^(?P<date>\d+-\d+-\d+ \d+:\d+:\d+)\s?.*$")
 
 content = inode_file.read()
 
@@ -19,8 +19,9 @@ for record in records:
     json_dict = dict(map(lambda x:x.split(':', 1), record))
     for key in json_dict:
         value = json_dict[key].strip()
-        if date_mask.match(value):
-            value = int(time.mktime(datetime.strptime(value, "%Y-%m-%d %H:%M:%S").timetuple()))
+        date = date_mask.match(value) # get rid of tailing timezone e.g. (CEST)
+        if date:
+            value = int(time.mktime(datetime.strptime(date.groupdict()['date'], "%Y-%m-%d %H:%M:%S").timetuple()))
         json_dict[key] = value
     json.dump(json_dict, output_file)
     output_file.write("\n")
