d93e1ccfb858ad3fbf8f64583cce4570dc3a9d62
Yu Jin <jinyu.bjut@gmail.com>
2013-04-10 00:58:31 +0200

    script for analyzing apps behaviour

---
+++ test/app_activity.js
@@ -0,0 +1,107 @@
+
+var cursor;
+var model;
+var selection;
+var all_apps_name = [];
+var all_apps_uid = [];
+var system_objects = {};
+var file_system_activity = {};
+var projection = {_id: 0, level: 0};
+
+cursor = db.packages.find();
+
+while(cursor.hasNext()) {
+    var record = cursor.next();
+    all_apps_name.push(record.name);
+    all_apps_uid.push(record.uid);
+}
+
+for (var i = 0; i < all_apps_uid.length; ++i) {
+    var target = all_apps_name[i];
+    file_system_activity[target] = {};
+    selection = {uid: all_apps_uid[i]};
+
+    cursor = db.fs_time.find(selection, {_id:0});
+    while(cursor.hasNext()) {
+        var record = cursor.next();
+        if (file_system_activity[target].hasOwnProperty(record.date) == false) {
+            file_system_activity[target][record.date] = {};
+            file_system_activity[target][record.date][record.file] = [record.activity];
+        } else {
+            if (file_system_activity[target][record.date].hasOwnProperty(record.file) == false) {
+                file_system_activity[target][record.date][record.file] = [record.activity];
+            } else {
+                file_system_activity[target][record.date][record.file].push(record.activity);
+            }
+        }
+    }
+}
+
+
+for (var i = 0; i < all_apps_name.length; ++i) {
+    var target = all_apps_name[i];
+    system_objects[target] = {};
+    selection = {msg: new RegExp('.*(' + target + "|" + target.substr(target.indexOf(".") + 1) + ").*", 'i')};
+
+    cursor = db.events.find(selection, projection);
+    while(cursor.hasNext()) {
+        var record = cursor.next();
+        if (system_objects[target].hasOwnProperty(record.object) == false) {
+            system_objects[target][record.object] = {};
+            system_objects[target][record.object][record.pid] = [record.msg];
+        } else {
+            if (system_objects[target][record.object].hasOwnProperty(record.pid) == false)
+                system_objects[target][record.object][record.pid] = [record.msg];
+            else
+                system_objects[target][record.object][record.pid].push(record.msg);
+        }
+    }
+
+    cursor = db.main.find(selection, projection);
+    while(cursor.hasNext()) {
+        var record = cursor.next();
+        if (system_objects[target].hasOwnProperty(record.object) == false) {
+            system_objects[target][record.object] = {};
+            system_objects[target][record.object][record.pid] = [record.msg];
+        } else {
+            if (system_objects[target][record.object].hasOwnProperty(record.pid) == false)
+                system_objects[target][record.object][record.pid] = [record.msg];
+            else
+                system_objects[target][record.object][record.pid].push(record.msg);
+        }
+    }
+
+    cursor = db.system.find(selection, projection);
+    while(cursor.hasNext()) {
+        var record = cursor.next();
+        if (system_objects[target].hasOwnProperty(record.object) == false) {
+            system_objects[target][record.object] = {};
+            system_objects[target][record.object][record.pid] = [record.msg];
+        } else {
+            if (system_objects[target][record.object].hasOwnProperty(record.pid) == false)
+                system_objects[target][record.object][record.pid] = [record.msg];
+            else
+                system_objects[target][record.object][record.pid].push(record.msg);
+        }
+    }
+}
+
+var save_buf = [];
+var _id = 0;
+for (var object in system_objects) {
+    if (system_objects.hasOwnProperty(object)) {
+        var buf = {};
+        buf._id = _id;
+        buf.app = object;
+        buf.detail = system_objects[object];
+        _id += 1;
+        save_buf.push(buf);
+    }
+}
+
+db.app_related_system_calls.insert(save_buf);
+
+printjson(file_system_activity);
+
+
+
