ed4a4cd7d208c0b7688ee6fc9e50a1391c72f088
Yu Jin <jinyu.bjut@gmail.com>
2013-04-11 17:18:48 +0200

    close to complete relevance query

--- test/activity_relevance.js
+++ test/activity_relevance.js
@@ -1,5 +1,5 @@
 
-var app_pattern = new RegExp('.*tencent.*', 'i');
+var app_pattern = new RegExp('.*(android.mms).*', 'i');
 var selection = {msg: app_pattern};
 
 var cursor = db.events.find(selection, {_id: 0, level: 0});
@@ -9,68 +9,90 @@ var map_reduce_selection = {};
 var object;
 var timestamp;
 
-//FIXME the following procedure has bugs
-var record;
-if (cursor.hasNext()) {
-    record = cursor.next();
-}
-
-while(cursor.hasNext()) {
-    object = record.object;
-    pid = record.pid;
-    timestamp = record.date;
-    record = cursor.next();
-    var upper_timewindow = record.date - timestamp > time_offset ? record.date : timestamp + time_offset;
-    map_reduce_selection.date = {$gte: (timestamp - time_offset), $lt: upper_timewindow};
-}
-
 var map = function() {
     var key = this.date;
     var value = {};
     value.object = [this.object];
     value.msg = [this.msg];
     value.pid= [this.pid];
-    value.score = [0];
+    value.score = [[0, 0, 0, 0]];
     emit(key, value);
 };
 
 var reduce = function(key, values) {
     var result = {object: [], msg: [], pid: [], score: []};
     values.forEach(function(value, index) {
-        result.object.push(value.object);
-        result.msg.push(value.msg);
-        result.pid.push(value.pid);
-        var score = 0;
-        score += Number(key) - Number(timestamp);
-        score += (value.object == object ? 5 : value.pid === pid ? 2 : 0);
-        score += (value.pid == pid ? 2 : 0);
-        score += (app_pattern.test(value.msg) ? 5 : 0);
-        result.score.push(score);
+        result.object.push(value.object[0]);
+        result.msg.push(value.msg[0]);
+        result.pid.push(value.pid[0]);
+        value.score[0][0] = (Number(timestamp) - Number(key)) % time_offset;
+        value.score[0][1] = (value.object[0] == object ? 1 : -1);
+        value.score[0][2] = (value.pid[0] == pid ? 1 : -1);
+        value.score[0][3] = (app_pattern.test(value.msg[0]) ? time_offset : 0);
+        result.score.push(value.score[0]);
     });
     return result;
 };
 
 var finalize = function(key, reduced_value) {
+    reduced_value.score.forEach(function(score, index) {
+        reduced_value.score[index] = (score[0] + score[1] + score[2] + score[3]) / 4;
+    });
     return reduced_value;
 };
 
-cursor = db.system.mapReduce(
-    map,
-    reduce,
-    {
-        out: "test_collection",
+var map_reduce_options;
+
+function doRelevanceQuery() {
+    var relevance_model;
+
+    relevance_model = db.system.mapReduce(
+        map,
+        reduce,
+        map_reduce_options 
+    );
+
+    relevance_model = db.main.mapReduce(
+        map,
+        reduce,
+        map_reduce_options 
+    );
+}
+
+var record = null;
+var is_finished = false;
+
+if (cursor.hasNext()) {
+    record = cursor.next();
+}
+
+while(!is_finished && record != null) {
+    object = record.object;
+    pid = record.pid;
+    timestamp = record.date;
+
+    if (cursor.hasNext()) {
+        record = cursor.next();
+        var upper_timewindow = record.date - timestamp > time_offset ? record.date : timestamp + time_offset;
+        map_reduce_selection.date = {$gt: (timestamp - time_offset), $lt: upper_timewindow};
+    } else { // if there is only one record, then simply use time offset for both time boundings
+        var upper_timewindow = timestamp + time_offset;
+        map_reduce_selection.date = {$gt: timestamp, $lt: upper_timewindow};
+        is_finished = true;
+    }
+    map_reduce_options = {
+        out: {merge: "test_collection"},
         query: map_reduce_selection,
         finalize: finalize,
         scope: {
             app_pattern: app_pattern,
             object: object,
             pid: pid,
-            timestamp: timestamp
+            timestamp: timestamp,
+            time_offset: time_offset
         }
-    }
-);
-
-printjson(cursor.find().next());
-
+    };
+    doRelevanceQuery();
+}
 
 
