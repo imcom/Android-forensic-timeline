2784d5171454cc98fde4eb5ce733e7cc3b4814f8
Yu Jin <jinyu.bjut@gmail.com>
2013-04-18 17:56:40 +0200

    fixed a map-reduce bug that single record does not go into reduce function added counting on system calls number based on time diff

--- test/activity_relevance.js
+++ test/activity_relevance.js
@@ -21,22 +21,83 @@ var timestamp;
 var map = function() {
     var key = this.date;
     var value = {};
+    var temporal_reference = 0, object_reference = 0, pid_reference = 0, msg_similarity = 0, msg_difference = 0;
+
+        var target_tokens = tokenize(this.object, this.msg);
+        if (reference_data !== null) {
+            if (reference_data.category[this.object] !== undefined) {
+                object_reference = reference_data.category[this.object].distribute;
+            }
+            if (reference_data.ids.indexOf(this.pid) !== -1) {
+                pid_reference = 1;
+            }
+            if (object_reference === 1 && pid_reference === 1) {
+                var reference_tokens = reference_data[this.object].tokens;
+            } else {
+                var reference_tokens = [];
+                for (var category in reference_data.category) {
+                    if (reference_data.category.hasOwnProperty(category)) {
+                        reference_data.category[category].tokens.forEach(function(token) {
+                            reference_tokens.push(token); // duplicate tokens but has no effects on result
+                        });
+                    }
+                }
+            }
+        } else {
+            var reference_buf = tokenize(object, msg);
+            var reference_id_tokens = reference_buf[1];
+            var reference_tokens = reference_buf[0];
+            object_reference = reference_tokens.indexOf(this.object) === -1 ? 0 : 1;
+            pid_reference = reference_id_tokens.indexOf(this.pid) === -1 ? 0 : 1;
+        }
+        var tokens_common = 0;
+        var tokens_unique = 0;
+        target_tokens[0].forEach(function(token) {
+            if (reference_tokens.indexOf(token) !== -1) {
+                tokens_common += 1;
+            } else {
+                tokens_unique += 1;
+            }
+        });
+        target_tokens[1].forEach(function(id_token) {
+            if (reference_data.ids.indexOf(id_token) !== -1) {
+                tokens_common += 1;
+            } else {
+                tokens_unique += 1;
+            }
+        });
+        msg_similarity = tokens_common / (target_tokens[0].length + target_tokens[1].length);
+        msg_difference = tokens_unique / (target_tokens[0].length + target_tokens[1].length);
+
     value.object = [this.object];
     value.msg = [this.msg];
     value.pid= [this.pid];
-    value.score = [[0, 0, 0, 0, 0]]; // temporal frequency, object reference, pid reference, msg similarity, msg difference
+    value.score = [[temporal_reference, object_reference, pid_reference, msg_similarity, msg_difference]]; // temporal distance, object reference, pid reference, msg similarity, msg difference
+    var count = {};
+    count[this.object] = 1;
+    value.frequency = {0: count};
     emit(key, value);
 };
 
 var reduce = function(key, values) {
-    var result = {object: [], msg: [], pid: [], score: []};
+    var result = {object: [], msg: [], pid: [], score: [], frequency: {}};
     values.forEach(function(value, index) {
         result.object.push(value.object[0]);
         result.msg.push(value.msg[0]);
         result.pid.push(value.pid[0]);
         var temporal_reference = 0, object_reference = 0, pid_reference = 0, msg_similarity = 0, msg_difference = 0;
         //TODO when value.object[0] is of some certain types, amplify the temporal_reference
-        temporal_reference = Math.abs(Number(timestamp) - Number(key)) / time_offset;
+        var time_diff = Number(timestamp) - Number(key);
+        if(result.frequency[time_diff] === undefined) {
+            result.frequency[time_diff] = value.frequency[0];
+        } else {
+            if (result.frequency[time_diff][value.object[0]] === undefined) {
+                result.frequency[time_diff][value.object[0]] = value.frequency[0][value.object[0]];
+            } else {
+                result.frequency[time_diff][value.object[0]] += value.frequency[0][value.object[0]];
+            }
+        }
+        temporal_reference = Math.abs(time_diff) / time_offset;
         temporal_reference = temporal_reference < 1 ? temporal_reference : 1;
 
         var target_tokens = tokenize(value.object[0], value.msg[0]);
@@ -97,7 +158,7 @@ var reduce = function(key, values) {
 
 var finalize = function(key, reduced_value) {
     reduced_value.score.forEach(function(score, index) {
-        reduced_value.score[index] = ((-1 * score[0]) + score[1] + score[2] + score[3] + (-1 * score[4])) / 5;
+        //reduced_value.score[index] = ((-1 * score[0]) + score[1] + score[2] + score[3] + (-1 * score[4])) / 5;
     });
     return reduced_value;
 };
