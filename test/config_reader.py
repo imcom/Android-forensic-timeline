0327866d07702952d6bc1a61c0a6f247fec97893
Yu Jin <jinyu.bjut@gmail.com>
2013-06-30 22:22:08 +0200

    removed old/extra testing scripts and source file keep two malware samples from dev

--- test/config_reader.py
+++
@@ -1,9 +0,0 @@
-
-
-f=open('../nodejs_server/som_config.js')
-lines = f.read()
-
-configs = dict(map(lambda x:x[0:-1].split('='), lines.strip().split('\n')))
-
-print configs.values()
-print int(configs.values()[0])
--- test/file_activity.js
+++
@@ -1,7 +0,0 @@
-
-var cursor;
-cursor = db.app_related_filesystem_activity.find({app:application_name});
-
-while(cursor.hasNext()) {
-    printjson(cursor.next());
-}
